d8e2b78d772c6b6b76517da4330738f2f4081dd7
amin <aminahissa8@gmail.com>
2025-06-11 14:09:21 +0300

    idk

--- src/Item.java
+++ src/Item.java
@@ -3,9 +3,19 @@ public abstract class Item {
     public String title;
     public int yearOfPublishing;
     public String topic;
+    boolean available;
+
+    public Item(int id,String title,int yearOfPublishing,String topic) {
+        this.id = id;
+        this.title = title;
+        this.yearOfPublishing = yearOfPublishing;
+        this.topic = topic;
+    }
+
 
 
 
     abstract void List();
     abstract void Return();
+    abstract void borrow();
 }
--- src/Project.java
+++ src/Project.java
@@ -1,7 +1,12 @@
 public class Project extends Item {
     int projectYear;
     String[] team;
-    boolean available;
+
+    public Project(int id,String title,int yearOfPublishing,String tobic,int projectYear,String[] team) {
+        super(id,title,yearOfPublishing,tobic);
+        this.projectYear=projectYear;
+        this.team = team;
+    }
 
     @Override
     void List(){
@@ -9,6 +14,11 @@ public class Project extends Item {
     }
     @Override
     void Return(){
+
+    }
+
+    @Override
+    void borrow() {
 
     }
 }
