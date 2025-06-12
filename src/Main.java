ffcceee46bcd5934cb2e855c5adf9906cfb63a53
mxm <broughtursintothis@gmail.com>
2025-06-12 17:29:05 +0300

    improve on the member class borrow/return system

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Date date1 = new Date(1,1,2020);
-        Date date2 = new Date(1,1,2025);
+        Date date1 = new Date(25, 7,2015);
+        Date date2 = new Date(12,6,2025);
         System.out.println(date1.TimeElapsedBetweenTwoDates(date1,date2));
     }
 }
--- src/Member.java
+++ src/Member.java
@@ -4,28 +4,42 @@ public class Member {
     int registrationNumber;
     String name;
     ArrayList<Item> BorrowedItems = new ArrayList<>();
+    ArrayList<Date> DateOfBorrowing = new ArrayList<>();
+    ArrayList<Date> DateOfReturning = new ArrayList<>();
+    private int numberOfBorrowedItems = 0;
 
-    Member(int registrationNumber, String name, ArrayList<Item> BorrowedItems){
+
+    Member(int registrationNumber, String name){
         this.registrationNumber = registrationNumber;
         this.name = name;
-        this.BorrowedItems = BorrowedItems;
     }
-    void BorrowItem(Item item){
-        if(BorrowedItems.size()<=3){
+    void BorrowItem(Item item, Date dateOfBorrowing){
+       /*, int day, int month,int year
+       if(BorrowedItems.size()<=3){
             BorrowedItems.add(item);
+            Date date = new Date(day,month,year);
             System.out.println("here you go!");
         }
         else
             System.out.println("you can't borrow more than three items." + "/n" +
-                                "you have to return an item first.");
-
+                                "you have to return an item first.");*/
+        BorrowedItems.add(item);
+        DateOfBorrowing.add(dateOfBorrowing);
+        numberOfBorrowedItems = numberOfBorrowedItems + 1;
 
     }
     //you have to save the date of borrowing/returning
 
-    void ReturnItem(Item item){
-        BorrowedItems.remove(item);
-        System.out.println("you have returned " + item.title);
+    /**you can make more hard by making multiple borrows/returns at the same time possible**/
+    void ReturnItem(Item item, Date dateOfReturning){
+        DateOfReturning.add(dateOfReturning);
+        numberOfBorrowedItems = numberOfBorrowedItems - 1;
+
+        //System.out.println("you have returned: " + item.title);
+    }
+
+    int getNumberOfBorrowedItems(){
+        return numberOfBorrowedItems;
     }
 
 }
