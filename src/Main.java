51dd339f0d33e69df218c2ebc187399184e336e7
mxm <broughtursintothis@gmail.com>
2025-06-13 15:15:16 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Date date1 = new Date(25, 7,2015);
+        Date date1 = new Date(25, 8,2015);
         Date date2 = new Date(12,6,2025);
         System.out.println(date1.TimeElapsedBetweenTwoDates(date1,date2));
     }
